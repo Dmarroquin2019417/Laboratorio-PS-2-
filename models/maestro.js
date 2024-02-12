6590a2eb21bcfa3d70d84bc03fc5a94c99d01909
Informatica <informatica@alumno.local>
2024-02-12 10:42:48 -0600

    se suben archivos de estudiantes

---
+++ models/maestro.js
@@ -0,0 +1,25 @@
+const { Schema, model} = require('mongoose');
+
+const MaestrosSchema = Schema ({
+    nombre: {
+        type: String,
+        required: [true, 'El nombre es obligatorio']
+    },
+    correo: {
+        type: String,
+        required: [true, 'El correo es obligatorio']
+    },
+    password: {
+        type: String,
+        required: [true, 'La password es obligatorio']
+    },
+    role:{
+        type: String,
+        required: true,
+        enum: ["TEACHER_ROLE", "STUDENT_ROLE"]
+    },
+});
+
+
+
+module.exports = model('Maestro', MaestrosSchema);
